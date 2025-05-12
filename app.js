dc61b9ee054e739dc4ee3bb0a3aedc598a90ab3a
Zack OConnor <zackary25@gmail.com>
2025-05-12 10:25:47 -0400

    Styled my site

--- app.js
+++ app.js
@@ -26,36 +26,46 @@ $(document).ready(function () {
         const description = data.weather[0].description
 
         const weatherHTMLElements = `
-            <h2>${cityName}</h2>
-            <p>Temperature: ${temperature}°F</p>
-            <p>Conditions: ${description}</p>
+          <h4 class="text-2xl font-bold mb-2"">${cityName}</h4>
+          <p class="text-xl">Temperature: <span class="font-semibold">${temperature}°F</span></p>
+          <p class="text-gray-600">Conditions: ${description}</p>
         `
         $('#weather-info').append(weatherHTMLElements)
 
         $.ajax({
           url: forecastURL,
           method: 'GET',
-          datayType: 'json',
+          dataType: 'json',
           success: function (forecastData) {
             console.log('Forecast Data:', forecastData)
 
             const processedDays = {}
+            const forecastContainer = $(
+              '<div class="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>'
+            )
+
             for (index in forecastData.list) {
               const forecastDate = new Date(forecastData.list[index].dt_txt)
               const dayOfMonth = forecastDate.getDate()
               if (!processedDays[dayOfMonth]) {
                 processedDays[dayOfMonth] = true
-                
-                
 
                 const forecastHTMLElements = `
-                    <h4>${dayOfMonth}</h4>
-                    <p>Temperature: ${Math.round(forecastData.list[index].main.temp)}°F</p>
-                    <p>Conditions: ${forecastData.list[index].weather[0].description}</p>
+                  <div class="bg-gray-200 rounded-md p-4">
+                    <h4 class="font-semibold mb-1">${dayOfMonth}</h4>
+                    <pclass="text-sm">Temperature: <span class="font-semibold">${Math.round(
+                      forecastData.list[index].main.temp
+                    )}°F</span></p>
+                    <p class="text-xs text-gray-600">Conditions: ${
+                      forecastData.list[index].weather[0].description
+                    }</p>
+                  </div>
                 `
-                $('#weather-info').append(forecastHTMLElements)
+                forecastContainer.append(forecastHTMLElements)
               }
             }
+
+            $('#weather-info').append(forecastContainer)
           },
 
           error: function (forecastError) {
